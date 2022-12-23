b53506dea128469aa7c8519d4cac9bac218e2f67
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2022-12-23 16:07:20 -0600

    made some changes from Jae's code to how the API is passed. It now successfully passes to the frontend

--- cast_api/views.py
+++ cast_api/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 import requests
 import os
 from dotenv import load_dotenv
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
+api_url = f'https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid={API_KEY}&units=imperial'
 
 # Create your views here.
 from rest_framework import generics
@@ -20,12 +23,7 @@ class ForecastDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Forecast.objects.all().order_by('city')
     serializer_class = ForecastSerializer
 
+@api_view(['GET'])
 def weather(request):
-    api_url = f'https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid={API_KEY}&units=imperial'
-    response = requests.get(api_url)
-    data = response.json()
-    json_response = json.dumps(data, indent=3)
-    print(data)
-    return render(request, "api.html", {'json_response': json_response})
-    # return JsonResponse(json_response, safe=False)
-    pass
+    data = requests.get(api_url).json()
+    return Response(data)
